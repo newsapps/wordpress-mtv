626c95959a454128aaff83b6caac323baf9af7fe
removedporn <86824510+removedporn@users.noreply.github.com>
2021-07-31 21:41:24 +0800

    Delete tests directory

--- tests/test_app/models.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace test_app\models;
-
-use mtv\models\Model;
-
-class TestModel extends Model {}
-
--- tests/test_app/views.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace test_app\views;
-
-function home() {
-    print "OK: Home";
-}
